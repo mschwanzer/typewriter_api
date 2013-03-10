971abb277cf19cef2c1a2f58de03dc9162ee6c22
Michael Schwanzer <michael.schwanzer@gmail.com>
2013-03-10 16:21:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TypewriterApi::Application.config.secret_token = 'fb14a132e9a3ef025d884b2d9ffda0913777d840cfe4a095e6f53cef32b5417b0bfbcc293aed5f78ecd5cbe03db5898fa732f152ab36b211f747c8a34e84fd24'
