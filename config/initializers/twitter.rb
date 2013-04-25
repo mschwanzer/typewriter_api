eb27474c4cdd13b58b1bc3c87ee623417bac2bff
Michael Schwanzer <michael.schwanzer@gmail.com>
2013-04-25 02:37:11 -0400

    Test without Settings

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,8 +1,3 @@
 Twitter.configure do |config|
-  if Setting.count > 0
-    config.consumer_key = Setting.last.twitter_app_id
-    config.consumer_secret = Setting.last.twitter_app_token
-    config.oauth_token = Setting.last.twitter_app_oauth
-    config.oauth_token_secret = Setting.last.twitter_app_oauth_token
-  end
+  
 end
